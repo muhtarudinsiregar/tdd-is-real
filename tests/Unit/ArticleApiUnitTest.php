0c2ab415362723457105c788e3a14df83062e870
muhtarudin <muhtarudin@javan.co.id>
2018-06-26 00:04:47 +0700

    Set env phpunit + add article unit test

---
+++ tests/Unit/ArticleApiUnitTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ArticleApiUnitTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $data = [
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraph
+        ];
+
+        $this->post(route('article.store'), $data)
+            ->assertStatus(201)
+            ->assertJson($data);
+    }
+}
