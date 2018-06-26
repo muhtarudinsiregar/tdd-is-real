5aaf8036f6c1aec0496180825e3383360e34fdd7
muhtarudin <muhtarudin@javan.co.id>
2018-06-26 09:29:54 +0700

    3. Create url in route file

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,15 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(
+    ['prefix' => 'v1'],
+    function () {
+        Route::resource('articles', ArticlesApiController::class);
+    }
+);
